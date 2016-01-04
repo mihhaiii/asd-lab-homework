686b501c6cc472b01db9d0fb4772a95c37ac2027
mihhaiii <mihaichihai03@gmail.com>
2016-01-05 01:56:35 +0200

    Create conundrum.cpp

---
+++ conundrum.cpp
@@ -0,0 +1,21 @@
+/*
+  https://open.kattis.com/problems/conundrum
+*/
+# include <bits/stdc++.h>
+
+using namespace std;
+
+# define forn(i,n) for(int i=0;i<(n);i++)
+# define debug(x) cerr << (#x) << " = " << (x) << endl
+
+string s;
+int ans = 0;
+
+int main()
+{
+    cin>>s;
+    forn(i,s.size())
+     if ( (i%3==0 && s[i]!='P') || (i%3==1 && s[i]!='E') || (i%3==2 && s[i]!='R') ) ans++;
+     cout<<ans;
+    return 0;
+}
